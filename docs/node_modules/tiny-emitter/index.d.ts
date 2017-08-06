01814dea4f9fbb3298415c29cf2b6c4b366248af
Slim Shady <dsslimshaddy@gmail.com>
2017-08-06 14:55:30 +0530

    remove cached builds n omdules

--- docs/node_modules/tiny-emitter/index.d.ts
+++
@@ -1,6 +0,0 @@
-export declare class EventEmitter {
-  on   (event: string, callback: Function, ctx?: any): EventEmitter;
-  once (event: string, callback: Function, ctx?: any): EventEmitter;
-  emit (event: string, ...args: any[]): EventEmitter;
-  off  (event: string, callback?: Function): EventEmitter;
-}
